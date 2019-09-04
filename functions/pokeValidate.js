337097951c7fb8ef71eb07d7fb38273e875f31d1
Sharkmare <34294231+Sharkmare@users.noreply.github.com>
2019-09-04 15:57:53 +0200

    Validates that given request type is valid/wanted

---
+++ functions/pokeValidate.js
@@ -0,0 +1,12 @@
+function pokeValidate(string) {
+switch(string)
+{
+case "pokemon":
+case "ability":
+case "type":
+case "move":
+case "item":
+case "pokemon-species":
+break;
+default: suffix="error"; return suffix
+}}
